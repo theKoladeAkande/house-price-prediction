2192c37528be3e06b77893be1b17fefbc2e37065
theKoladeAkande <akandekolade@gmail.com>
2020-01-31 08:58:29 +0100

    Deployment on Heroku
    installed gunicorn, added under ml_requirements
    set ml_api run.py to prdocution config

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -1,9 +1,9 @@
 #run.py
 """ Entry point """
 from api.app import create_app
-from api.config import DevelopmentConfig
+from api.config import DevelopmentConfig, ProductionConfig
 
-application = create_app(DevelopmentConfig)
+application = create_app(ProductionConfig)
 
 
 if __name__ == '__main__':
